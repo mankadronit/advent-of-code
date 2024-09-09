2a12614ecdc1fc151af58933c4a02368552da0c0
Ronit Mankad <mankadronit.rm@gmail.com>
2024-09-09 16:34:38 -0400

    Update Solution: 2023-Day01 Add solution for Part 2

--- 2023/day01/index.js
+++ 2023/day01/index.js
@@ -1,28 +1,64 @@
 const fs = require("fs");
 
+const dic = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+};
+
+function isDigit(char) {
+    return !isNaN(Number.parseInt(char));
+}
+
+function findNumberWords(line) {
+    ans = {};
+    for (let i = 0; i < line.length - 1; i++) {
+        for (let j = i + 1; j < line.length; j++) {
+            let word = line.substring(i, j + 1);
+            if (word in dic) {
+                if (!("first" in ans)) {
+                    ans["first"] = [dic[word], i];
+                } else {
+                    ans["last"] = [dic[word], line.length - i];
+                }
+            }
+        }
+    }
+
+    if (!("last" in ans) & ("first" in ans)) {
+        ans["last"] = [ans["first"][0], line.length - ans["first"][1]];
+    }
+
+    return ans;
+}
+
 function findNumber(line, reverse = false) {
     if (reverse) {
         line = line.split("").reverse().join("");
     }
 
     for (let i = 0; i < line.length; i++) {
-        let curr = Number.parseInt(line[i]);
-
-        if (!isNaN(curr)) {
-            return `${curr}`;
+        if (isDigit(line[i])) {
+            return [line[i], i];
         }
     }
 }
 
 function readFile(filePath, encoding = "utf-8") {
-    fs.readFile(filePath, encoding, function(err, data) {
+    fs.readFile(filePath, encoding, function (err, data) {
         if (err) {
             console.log(err);
         }
 
         let sum = 0;
 
-        data.split("\n").forEach(function(line) {
+        data.split("\n").forEach(function (line) {
             if (!line) {
                 return;
             }
@@ -33,10 +69,33 @@ function readFile(filePath, encoding = "utf-8") {
             let val = first + last;
 
             if (isNaN(val)) {
-                let a = findNumber(line);
-                let b = findNumber(line, (reverse = true));
+                let a = findNumber(line) || Infinity;
+                let b = findNumber(line, (reverse = true)) || -Infinity;
 
-                val = a + b;
+                let w = findNumberWords(line);
+                let x, y;
+
+                if ("first" in w === true) {
+                    if (a[1] < w["first"][1]) {
+                        x = a[0];
+                    } else {
+                        x = w["first"][0];
+                    }
+                } else {
+                    x = a[0];
+                }
+
+                if ("last" in w === true) {
+                    if (b[1] < w["last"][1]) {
+                        y = b[0];
+                    } else {
+                        y = w["last"][0];
+                    }
+                } else {
+                    y = b[0];
+                }
+
+                val = String(x) + String(y);
             }
 
             sum += Number(val);
