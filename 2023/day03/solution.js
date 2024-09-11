bd5877e21c46e5dab1d757a9e1c108cabb1cc005
Ronit Mankad <mankadronit.rm@gmail.com>
2024-09-11 15:43:42 -0400

    Add Solution: 2023-Day03 Part 1

---
+++ 2023/day03/solution.js
@@ -0,0 +1,108 @@
+const fs = require("fs");
+const os = require("os");
+
+const content = fs.readFileSync("input.txt", "utf-8").trim().split(os.EOL);
+
+// Parse the input as a matrix
+function getContentMatrix(content) {
+    const contentMatrix = [];
+
+    content.forEach((row) => {
+        if (!row) {
+            return;
+        }
+        contentMatrix.push(row.split(""));
+    });
+
+    return contentMatrix;
+}
+
+const matrix = getContentMatrix(content);
+const TOTAL_ROWS = matrix.length;
+const TOTAL_COLUMNS = matrix[0].length;
+
+// Loop a row of the matrix until the current character is non-integer
+function loopUntilNotNumber(matrix, row, col) {
+    while (isNaN(Number.parseInt(matrix[row][col])) & (col <= TOTAL_COLUMNS)) {
+        col += 1;
+    }
+
+    return col;
+}
+
+// Loop a row of the matrix until the current character is an integer
+function loopUntilNumber(matrix, row, col) {
+    let c = col;
+    while (!isNaN(Number.parseInt(matrix[row][c])) & (c <= TOTAL_COLUMNS)) {
+        c += 1;
+    }
+
+    return c;
+}
+
+// Check if the current number is valid by checking all surrounding cells for a special
+// character.
+function checkValidity(matrix, row, start_col, end_col) {
+    for (let r = row - 1; r <= row + 1; r++) {
+        for (let c = start_col - 1; c < end_col + 1; c++) {
+            if (
+                (r >= 0) &
+                (r <= TOTAL_ROWS - 1) &
+                (c >= 0) &
+                (c <= TOTAL_COLUMNS - 1)
+            ) {
+                if (isNaN(Number(matrix[r][c])) & (matrix[r][c] !== ".")) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+// Loop through the matrix and get all valid parts
+function getValidParts(matrix) {
+    let row = 0;
+    let col = 0;
+
+    let validParts = [];
+
+    // Loop through entire matrix
+    while (row <= TOTAL_ROWS - 1) {
+        while (col <= TOTAL_COLUMNS - 1) {
+            // Loop Through Row until Number is discovered
+            let start_col = loopUntilNotNumber(matrix, row, col);
+            let end_col;
+            if (start_col <= TOTAL_COLUMNS - 1) {
+                // Now that first number is found, loop until a non-number is discovered
+                end_col = loopUntilNumber(matrix, row, start_col + 1);
+            } else {
+                end_col = start_col;
+            }
+
+            // Conver to number
+            let num = Number(matrix[row].slice(start_col, end_col).join(""));
+
+            // Check Validity
+            if (!isNaN(num) && checkValidity(matrix, row, start_col, end_col)) {
+                validParts.push(num);
+            }
+
+            col = end_col + 1;
+        }
+
+        row += 1;
+        col = 0;
+    }
+
+    return validParts;
+}
+
+const validParts = getValidParts(matrix);
+
+const ans = validParts.reduce((previous, current) => {
+    return previous + current;
+}, 0);
+
+console.log("Answer: ", ans);
