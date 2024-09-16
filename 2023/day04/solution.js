1cfcb301a4842110ae30c42945ac9086d4063299
Ronit Mankad <mankadronit.rm@gmail.com>
2024-09-15 23:50:18 -0400

    Add Solution: 2023-day04 Part 2 (wip)

--- 2023/day04/solution.js
+++ 2023/day04/solution.js
@@ -4,8 +4,13 @@ const content = fs.readFileSync("input.txt", "utf-8").trim().split("\n");
 
 console.log(content);
 
-let total_points = 0;
+// Instantiate return answer
+const cards_won = {};
 
+// Store all card
+const cards = {};
+
+// Iterate through the content, parse card and add to Cards
 content.forEach((card) => {
     if (card) {
         win_map = {};
@@ -20,8 +25,6 @@ content.forEach((card) => {
                 .map((n) => Number(n.trim())),
         );
 
-        win_map[card_id] = winning_numbers;
-
         let card_numbers = card
             .split(":")[1]
             .split("|")[1]
@@ -30,27 +33,55 @@ content.forEach((card) => {
             .filter((n) => n.trim() !== "")
             .map((n) => Number(n.trim()));
 
-        let matched_cards = 0;
-        let points = 0;
-
-        console.log("CARD NUMBERS:", card_numbers);
-        card_numbers.forEach((num) => {
-            if (win_map[card_id].has(num)) {
-                matched_cards += 1;
-                if (matched_cards === 1) {
-                    points = 1;
-                } else if (matched_cards === 2) {
-                    points = 2;
-                } else {
-                    points *= 2;
-                }
-            }
-        });
-
-        console.log("CARD ", card_id, " Points: ", points);
-
-        total_points += points;
+        cards[card_id] = [winning_numbers, card_numbers];
     }
 });
 
-console.log(total_points);
+// Add the default card ids with count 1
+for (const card_id in cards) {
+    cards_won[card_id] = 1;
+}
+
+function getMatchedCards(card_id) {
+    if (!(card_id in cards)) {
+        cards[card_id] = [new Set(), []];
+    }
+    const winning_numbers = cards[card_id][0];
+    const card_numbers = cards[card_id][1];
+
+    const matched_cards = Array.from(card_numbers).filter((num) =>
+        winning_numbers.has(Number(num)),
+    );
+
+    return matched_cards.length;
+}
+
+function collectCards(card_id, matched_cards) {
+    if (matched_cards > 0) {
+        const cardsToCollect = Array.from(
+            { length: matched_cards },
+            (_, i) => Number(card_id) + i + 1,
+        );
+
+        cardsToCollect.forEach((card) => {
+            cards_won[card] = cards_won[card] || 0;
+            cards_won[card] += 1;
+        });
+
+        cardsToCollect.forEach((card) =>
+            collectCards(card, getMatchedCards(card)),
+        );
+    }
+}
+
+for (const card in cards) {
+    collectCards(card, getMatchedCards(card));
+}
+
+let ans = 0;
+
+for (let card in cards_won) {
+    ans += cards_won[card];
+}
+
+console.log(ans);
